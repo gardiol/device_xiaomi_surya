53560427b23c63109d200f6343ed28846517c7ff
Willy Gardiol <willt2gardiol.org>
2021-01-03 21:35:36 +0100

    Fixed low volume recording issue

--- product_prop.mk
+++ product_prop.mk
@@ -24,7 +24,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicerec=true \
-    persist.vendor.audio.fluence.audiorec=true
+    persist.vendor.audio.fluence.audiorec=false
 
 # Smart fps switching
 PRODUCT_PRODUCT_PROPERTIES += \
