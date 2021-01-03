e2e8c5da4ca1f4b7ca1d26fd8704650dc32657a8
Willy Gardiol <willt2gardiol.org>
2021-01-03 21:36:31 +0100

    Removed havoc and added lineage lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_surya.mk
+    $(LOCAL_DIR)/lineage_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_surya-user \
-    havoc_surya-userdebug \
-    havoc_surya-eng
+    lineage_surya-user \
+    lineage_surya-userdebug \
+    lineage_surya-eng
