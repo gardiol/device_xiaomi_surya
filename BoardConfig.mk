b6e10718928ddf76c89ed2d516140fd038c3c6a4
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-11 21:22:49 +0530

    surya: Switch to permissive for now
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,8 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_surya
 # Kernel
 TARGET_KERNEL_CONFIG := surya_defconfig
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
