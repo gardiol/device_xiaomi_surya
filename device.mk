18a2579e1702e25712959cd4629b4ba512cbe121
Willy Gardiol <willy@gardiol.org>
2021-06-11 14:39:23 +0200

    surya: imported fingerprint service
    Imported fingerprint service from sm6150-common, and renamed to surya to
    avoid conflicts, but adapted to surya specifics:
    * Modificed source to work with specicific surya hardware
    * Added fingerprint extensions
    * Added HIDL stuff
    
    Surya fingerprint is similar but has peculiar hardware activation
    feature since it's located on power button instead of on back.
    
    Change-Id: I694d1428a712ff5f1131bd655babec32a149108b

--- device.mk
+++ device.mk
@@ -129,6 +129,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Doze
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_surya
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
