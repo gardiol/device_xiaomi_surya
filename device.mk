052a7936d117319897a2db55345ea612eb81bb2e
Willy Gardiol <willy@gardiol.org>
2021-06-21 10:08:02 +0200

    surya: setup partition scheme
    Configure static partitions:
    * Set SUPER partition size to 8192MB
    * Set RECUVERY partition size to 128MB
    * Set BOOT partition size to 128MB
    * Set DTBO partition size to 24MB
    * Set CACHE partition size to 384MB
    
    Initialize DYNAMIC partition scheme:
    * Set dynamic partition size to 8188MB (SUPER - 4MB)
    * Set reserved free space for SYSTEM at 1800MB
    * Set reserved free space for SYSTEM_EXT at 100MB
    * Set reserved free space for PRODUCT at 1800MB
    * Set reserved free space for VENDOR at 100MB
    
    Free spaces have been calculated to fit additional user flashed
    packages.
    
    Change-Id: I8a54d0c651e291e1d67a60387c7642cf0116179f

--- device.mk
+++ device.mk
@@ -303,6 +303,10 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-system
 
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Perf
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
