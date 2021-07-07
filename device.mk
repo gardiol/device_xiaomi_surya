afd6bb99c9b1f6f0149816c8615b125a9b4768a6
Willy Gardiol <willy@gardiol.org>
2021-07-07 09:40:46 +0200

    surya: renamed light service for surya
    Change-Id: I602ee9dc239322f12fe99d8fc0349d8bdc4f337c

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.davinci
+    android.hardware.light@2.0-service.surya
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
