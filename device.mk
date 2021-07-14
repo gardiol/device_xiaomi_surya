13ea20edb9db1f60c77371386af054cbeb36bd99
Willy Gardiol <willy@gardiol.org>
2021-07-14 13:37:21 +0200

    surya: rename fingerprint service

--- device.mk
+++ device.mk
@@ -160,8 +160,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.davinci \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
+    android.hardware.biometrics.fingerprint@2.1-service.surya \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.surya
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
